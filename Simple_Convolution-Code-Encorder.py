c9545fb42e3f0dd19b30b0d81deb4bd648a7d452
JanaBan99 <95229070+JanaBan99@users.noreply.github.com>
2023-11-27 18:35:55 +0530

    Update Simple_Convolution-Code-Encorder.py

--- Simple_Convolution-Code-Encorder.py
+++ Simple_Convolution-Code-Encorder.py
@@ -1,7 +1,12 @@
-
-input_bits = [1, 0, 1, 0, 0, 0, 1]
+temp = []
+user_input_bits = input("Enter the data bits you want to encode:")
+input_bits = []
+input_bits = [int(bit) for bit in user_input_bits]
+input_bits.extend([0, 0, 0])
 shift_register = [0, 0, 0]
 Convolutional_encoded_bits = []
+n = len(input_bits)
+print("print n value:", n)
 
 def shift_bits():
     shift_register[2] = shift_register[1]
@@ -13,13 +18,21 @@ def calculate():
     c3 = shift_register[0] ^ shift_register[1]
     Convolutional_encoded_bits.extend([c1, c2, c3])
     shift_bits()
-
-i = 6
-while i >= 0:
+    #Convolutional_encoded_bits.extend([c1,c2])
+    #print ("i value:", i)
+    #print ("c1 value:", c1)
+    #print ("c2 value:", c2)
+    #print ("c3 value:", c3)
+    
+    
+i =  0
+while i <= n-1 :
     temp = input_bits[i]
     shift_register[0] = temp
-    input_bits.pop(i)
-    i -= 1
+    #input_bits.pop(i)
+    i += 1
     calculate()
+#else:
+    #print("Finished encoding bits")
 
 print("Convolutional-encoded_bits:", Convolutional_encoded_bits)
